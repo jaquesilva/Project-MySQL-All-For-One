6d06b3225b99cb7b755886466049737641fe4802
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-10-20 10:12:12 -0300

    Fixes requirement 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,3 +1,6 @@
-SELECT * FROM purchase_orders
-WHERE supplier_id >= 1 AND supplier_id <= 3
-ORDER BY supplier_id ASC;
+-- SELECT supplier_id FROM purchase_orders
+-- WHERE supplier_id BETWEEN 1 AND 3
+-- ORDER BY supplier_id ASC;
+
+SELECT supplier_id FROM northwind.purchase_orders
+WHERE supplier_id BETWEEN 1 AND 3;
